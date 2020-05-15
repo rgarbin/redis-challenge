3ec50e29239aab89a82f06b05dee3302b1c11bab
Rafael Garbin <rafael.garbin@syntonic.com>
2020-05-15 12:10:56 -0300

    initial commit

---
+++ src/main/java/com/redischallenge/redischallenge/RedisChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.redischallenge.redischallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisChallengeApplication.class, args);
+	}
+
+}
